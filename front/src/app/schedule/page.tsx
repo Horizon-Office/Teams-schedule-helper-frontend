fb2836a5973134b2a81756b99f6661ef51a685ad
Max Ivanov <ima20070111@gmail.com>
2025-12-30 17:50:57 +0200

    imperative mood(schedule page): add structure for schedule page

--- front/src/app/schedule/page.tsx
+++ front/src/app/schedule/page.tsx
@@ -1,19 +1,29 @@
 import ScheduleTable from '@/components/schedule/ScheduleTable';
+import { SidebarRenderer } from '@/components/SidebarRenderer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <div className="container mx-auto px-4">
-        <header className="mb-8">
-          <h1 className="text-3xl font-bold text-gray-900 mb-2">
-            Університетський розклад занять
-          </h1>
-          <p className="text-gray-600">
-            Система управління розкладом для університету
-          </p>
-        </header>
-        <ScheduleTable />
+    <section>
+      <SidebarRenderer page="schedule" />
+      <div style={{paddingLeft: "249px"}} className="
+        flex
+        w-full
+        text-black
+        ">
+        <main className="min-h-screen bg-gray-50 py-8">
+          <div className="container mx-auto px-4">
+            <header className="mb-8">
+              <h1 className="text-3xl font-bold text-gray-900 mb-2">
+                Університетський розклад занять
+              </h1>
+              <p className="text-gray-600">
+                Система управління розкладом для університету
+              </p>
+            </header>
+            <ScheduleTable />
+          </div>
+        </main>
       </div>
-    </main>
+    </section>
   );
 }
